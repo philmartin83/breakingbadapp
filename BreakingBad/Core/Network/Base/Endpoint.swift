0d5fedbe7e5b17914a63c9114b30ccc97ca81b9c
Phil Martin <phil.martin1983@gmail.com>
2022-06-26 20:01:23 +0100

    Initial commit for starter project

---
+++ BreakingBad/Core/Network/Base/Endpoint.swift
@@ -0,0 +1,30 @@
+//
+//  Endpoint.swift
+//  AnimalCrossingFun
+//
+//  Created by Philip Martin on 16/05/2022.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+}
+
+extension Endpoint {
+    var baseURL: String {
+        return "https://acnhapi.com/v1a/"
+    }
+}
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
---
+++ BreakingBad/Home/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BreakingBad
+//
+//  Created by Philip Martin on 26/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+
+}
+
